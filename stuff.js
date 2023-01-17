aedf32b056e7d4850ab040dde114f7896047d35e
Nouhi <67205946+NouhiDev@users.noreply.github.com>
2023-01-17 16:19:14 +0100

    a

--- stuff.js
+++ stuff.js
@@ -1,4 +1,5 @@
-$(window).on("load",function(){
+$(window).on("load",async function(){
+    await delay(2000);
     $(".loader").fadeOut(1000);
     delay(1000).then(() => $(".content").fadeIn(1000));
 })
