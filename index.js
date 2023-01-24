6e34f0a52b088915fc8fcd25fc161cd4f8a4308a
Nouhi <67205946+NouhiDev@users.noreply.github.com>
2023-01-24 17:29:44 +0100

    Restyling

---
+++ index.js
@@ -0,0 +1,21 @@
+$(window).on("load", async function () {
+    $(".loader").fadeOut(1000);
+    delay(1000).then(() => $(".content").fadeIn(1000));
+  });
+  
+  function delay(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+  }
+  
+  var typed = new Typed(".auto-type", {
+    strings: ["Developer", "Game Designer", "Graphic Designer"],
+    typeSpeed: 150,
+    backSpeed: 100,
+    startDelay: 1000,
+    loop: true,
+  });
+  
+  document.getElementById("btn").onclick = function () {
+    location.href = "https://nouhidev.github.io/projects/";
+  };
+  
