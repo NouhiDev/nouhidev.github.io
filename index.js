3380bb952ea8c46438459b7b86c3acc01480a743
nouhi <wolfgraphics.business@gmail.com>
2023-07-20 19:13:01 +0200

    global ndev script

--- index.js
+++ index.js
@@ -1,30 +1,11 @@
-$(window).on("load", async function () {
-    $(".loader").fadeOut(1000);
-    delay(1000).then(() => $(".content").fadeIn(1000));
-  });
-  
-  function delay(time) {
-    return new Promise((resolve) => setTimeout(resolve, time));
-  }
-  
-  var typed = new Typed(".auto-type", {
-    strings: ["Developer", "Game Designer", "Graphic Designer"],
-    typeSpeed: 150,
-    backSpeed: 100,
-    startDelay: 1000,
-    loop: true,
-  });
-  
-  document.getElementById("btn").onclick = function () {
-    location.href = "https://nouhidev.github.io/projects/";
-  };
-  
-/* Setup global Navbar */ 
-const NAV_URL = "https://nouhi.dev/assets/html-templates/navbar.txt";
-
-fetch(NAV_URL)
-  .then( r => r.text() )
-  .then( t => {
-    var whereToInject = document.getElementsByTagName("header")[0];
-    whereToInject.innerHTML += t;
+var typed = new Typed(".auto-type", {
+  strings: ["Developer", "Game Designer", "Graphic Designer"],
+  typeSpeed: 150,
+  backSpeed: 100,
+  startDelay: 1000,
+  loop: true,
 });
+
+document.getElementById("btn").onclick = function () {
+  location.href = "https://nouhidev.github.io/projects/";
+};
